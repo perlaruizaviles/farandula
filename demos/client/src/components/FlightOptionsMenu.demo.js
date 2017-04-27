bc0263397febdc3686276677cd17c1c805e36fb9
Eduardo Acuña Yeomans <eacuna@nearsoft.com>
2017-04-27 11:00:57 -0700

    add flight options menu layout

--- demos/client/src/components/FlightOptionsMenu.demo.js
+++ demos/client/src/components/FlightOptionsMenu.demo.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import FlightOptionsMenu from './FlightOptionsMenu';
-import defaultFlightSettings from '../data/defaultFlightSettings';
+//import defaultFlightSettings from '../data/defaultFlightSettings';
+import {Map} from 'immutable';
 import flightOptions from '../data/flightOptions';
 
+const settings = Map({
+  cabin: 'premium-economy',
+  passengers: Map({
+    'adult':       1,
+    'senior':      0,
+    'youth':       0,
+    'child':       0,
+    'seat-infant': 0,
+    'lap-infant':  0
+  })
+});
 
 export default () => (
-  <FlightOptionsMenu settings={defaultFlightSettings}
+  <FlightOptionsMenu settings={settings}
                      options={flightOptions}
   />
 );
--- demos/client/src/components/FlightOptionsMenu.js
+++ demos/client/src/components/FlightOptionsMenu.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {findOptionById} from '../util/flightOptions';
 import * as inf from 'inflection';
-import {Dropdown} from 'semantic-ui-react';
-
+import {Dropdown, Grid} from 'semantic-ui-react';
 import {countPassengers} from '../util/flightSettings';
 
 const settingsString = (settings, options) => {
@@ -38,19 +37,38 @@ const FlightOptionsMenu = ({settings, options, ...props}) => (
     <Dropdown.Menu>
       <Dropdown.Header content="cabin"/>
       <Dropdown.Item>
-        <Dropdown text={settings.get('cabin')} fluid closeOnBlur={false}>
+        <Dropdown text={inf.titleize(findOptionById(options, 'cabin', settings.get('cabin')).get('name'))}
+                  fluid closeOnBlur={false}>
           <Dropdown.Menu>
-            <Dropdown.Item>
-              Flow
-            </Dropdown.Item>
+            {options.get('cabin')
+              .map(cabin =>
+                (<Dropdown.Item key={cabin.get('id')}>
+                  {inf.titleize(cabin.get('name'))}
+                  </Dropdown.Item>))}
           </Dropdown.Menu>
         </Dropdown>
       </Dropdown.Item>
       <Dropdown.Divider/>
       <Dropdown.Header content="passengers"/>
-      <Dropdown.Item text="foo"/>
-      <Dropdown.Item text="bar"/>
-      <Dropdown.Item text="baz"/>
+      <Dropdown.Item>
+        <Grid columns='equal'>
+          {settings.get('passengers')
+            .map((count,id) =>
+              (<Grid.Row key={id}>
+                  <Grid.Column width={7}>
+                    {inf.titleize(inf.pluralize(findOptionById(options,'passengers',id).get('name')))}
+                  </Grid.Column>
+                  <Grid.Column><button>-</button></Grid.Column>
+                  <Grid.Column>{count}</Grid.Column>
+                  <Grid.Column><button>+</button></Grid.Column>
+                  <Grid.Column>
+                    {findOptionById(options, 'passengers', id).get('ageRange').get(0)} -
+                    {findOptionById(options, 'passengers', id).get('ageRange').get(1)}
+                  </Grid.Column>
+                </Grid.Row>
+              )).valueSeq()}
+        </Grid>
+      </Dropdown.Item>
     </Dropdown.Menu>
   </Dropdown>
 );
