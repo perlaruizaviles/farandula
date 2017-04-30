d1a55416fc4c721b511b1e3fac9265480c542ce3
abustamante <abustamante@nearsoft.com>
2017-04-29 19:26:07 -0700

    clean comments

--- demos/client/src/components/TripMenu.demo.js
+++ demos/client/src/components/TripMenu.demo.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TripMenu from './TripMenu';
 
-
 export default () => (
   <TripMenu />
 );
--- demos/client/src/components/TripMenu.js
+++ demos/client/src/components/TripMenu.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
 
-//onClick={e => onCabinClick(id)}
 const TripMenu = ({activeItem, handleItemClick}) => (
     <Menu text>
         <Menu.Item name='roundTrip' active={activeItem === 'roundTrip'}
@@ -10,22 +9,5 @@ const TripMenu = ({activeItem, handleItemClick}) => (
                    onClick={(e, {name}) => handleItemClick(name)} />
     </Menu>
 );
-
-// class TripMenu extends Component {
-//   state = { activeItem: 'roundTrip' }
-//
-//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-//
-//   render() {
-//     const { activeItem } = this.state
-//
-//     return (
-//       <Menu text>
-//         <Menu.Item name='roundTrip' active={activeItem === 'roundTrip'} onClick={this.handleItemClick} />
-//         <Menu.Item name='oneWay' active={activeItem === 'oneWay'} onClick={this.handleItemClick} />
-//       </Menu>
-//     )
-//   }
-// }
 
 export default TripMenu;
