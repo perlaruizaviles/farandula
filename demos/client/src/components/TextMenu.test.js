dada9b07cc31ced210393cbb2cc338214715abd1
Eduardo Acuña Yeomans <eacuna@nearsoft.com>
2017-05-02 17:57:20 -0700

    fix text menu rendering tests

--- demos/client/src/components/TextMenu.test.js
+++ demos/client/src/components/TextMenu.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 import TextMenu from './TextMenu';
+import {Map, List} from 'immutable';
 
 it('renders without crashing (shallow)', () => {
-  shallow(<TextMenu/>);
+  shallow(<TextMenu options={List(['x'])} selected={'x'} />);
 });
 
 it('renders without crashing (full)', () => {
-  mount(<TextMenu/>);
+  mount(<TextMenu options={List(['x'])} selected={'x'} />);
 });
