b08e73d95434f420843eb0e6c49ea4a6ea75df2d
Eduardo Acuña Yeomans <eacuna@nearsoft.com>
2017-05-02 17:56:48 -0700

    remove action log

--- demos/client/src/reducers/travelConfig.js
+++ demos/client/src/reducers/travelConfig.js
@@ -1,7 +1,6 @@
 import {Map} from 'immutable';
 
 const travelConfig = (state = Map({}), action) => {
-  console.log(action);
   switch (action.type) {
     case 'CHANGE_TRAVEL_TYPE':
       return state.set('type', action.value);
