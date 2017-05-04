d3c5185cd899f50d08c9a51cd978d9c75c31cc42
Luis Daniel Alducin Acevedo <luisalduucin@users.noreply.github.com>
2017-05-03 20:07:52 -0500

    Added Android Java 8 capabilities

--- farandula-android/FarandulaMobile/app/src/main/java/com/nearsoft/farandulamobile/MainActivity.java
+++ farandula-android/FarandulaMobile/app/src/main/java/com/nearsoft/farandulamobile/MainActivity.java
@@ -3,11 +3,17 @@ package com.nearsoft.farandulamobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nearsoft.farandula.Luisa;
+import com.nearsoft.farandula.flightmanagers.sabre.SabreFlightManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SabreFlightManager tripManager = new SabreFlightManager();
+        Luisa.setSupplier(() -> tripManager);
     }
 }
