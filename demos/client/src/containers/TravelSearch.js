96eeca6499dc05c87dede3fdce88165d23d087a7
Jhonatan Laguna <jhonlpdev@gmail.com>
2017-05-04 15:38:57 -0700

    Quick fix

--- demos/client/src/containers/TravelSearch.js
+++ demos/client/src/containers/TravelSearch.js
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux';
 import TravelSearch from '../components/TravelSearch';
-import {changeTravelType, changeTravelDate} from '../actions/travelConfig';
+import {changeTravelType, changeTravelDate, changeTravelFrom, changeTravelTo} from '../actions/travelConfig';
 
 export default connect(
   state => {
