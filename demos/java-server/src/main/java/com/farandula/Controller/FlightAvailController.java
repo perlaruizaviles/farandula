4e38ca8a17447bdd1f0e02071f4bb39e627ef4ae
Antonio <antoniohernandez@Antonio-2.local>
2017-05-05 11:33:01 -0600

    Initial structure

---
+++ demos/java-server/src/main/java/com/farandula/Controller/FlightAvailController.java
@@ -0,0 +1,19 @@
+package com.farandula.Controller;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by antoniohernandez on 5/4/17.
+ */
+
+@RestController
+public class FlightAvailController {
+
+//    @RequestMapping("/api/avails")
+//    public Response getAvailableFlights(@Param("iataDepart") String iataDepart,@Param("iataDestiny")String iataDestiny){
+//
+//    }
+
+}
