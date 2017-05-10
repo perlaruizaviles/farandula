469dfddf605884df7a652828f72015323af4412b
abustamante <abustamante@nearsoft.com>
2017-05-10 10:52:27 -0700

    Clean code (Remove old result-page)

--- demos/client/src/router.js
+++ demos/client/src/router.js
@@ -6,8 +6,6 @@ import Main from './components/Main';
 import Welcome from './components/Welcome';
 import ComponentShowcase from './components/ComponentShowcase';
 
-import Results from './components/ResultsPage';
-
 const makeRouter = store => {
   let history = syncHistoryWithStore(browserHistory, store);
   return (
@@ -17,8 +15,6 @@ const makeRouter = store => {
         <Route path="/home" component={Welcome}/>
         <Route path="/components" component={ComponentShowcase}>
         </Route>
-        <IndexRedirect to="/results"/>
-        <Route path="/results" component={Results}/>
       </Route>
     </Router>
   )
