d8c515f0efe567774a646370038d25cca704c652
abustamante <abustamante@nearsoft.com>
2017-05-10 14:31:55 -0700

    Modifies months

--- demos/client/src/components/TravelerForm.js
+++ demos/client/src/components/TravelerForm.js
@@ -10,18 +10,18 @@ const ladas = [
 ];
 
 const months = [
-    { text: 'Enero', value: '1'},
-    { text: 'Febrero', value: '2'},
-    { text: 'Marzo', value: '3'},
-    { text: 'Abril', value: '4'},
-    { text: 'Mayo', value: '5'},
-    { text: 'Junio', value: '6'},
-    { text: 'Julio', value: '7'},
-    { text: 'Agosto', value: '8'},
-    { text: 'Septiembre', value: '9'},
-    { text: 'Octubre', value: '10'},
-    { text: 'Noviembre', value: '11'},
-    { text: 'Diciembre', value: '12'},
+    { text: 'January', value: '1'},
+    { text: 'February', value: '2'},
+    { text: 'March', value: '3'},
+    { text: 'April', value: '4'},
+    { text: 'May', value: '5'},
+    { text: 'June', value: '6'},
+    { text: 'July', value: '7'},
+    { text: 'August', value: '8'},
+    { text: 'September', value: '9'},
+    { text: 'October', value: '10'},
+    { text: 'November', value: '11'},
+    { text: 'December', value: '12'},
 ];
 
 const days = [  {text:'01', value:'1'},
