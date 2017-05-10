0301c45b3d36387c2331a91c427883e42ca095a3
juan <jluque@nearsoft.com>
2017-05-10 14:33:32 -0700

    Changes the flightresults test

--- demos/client/src/components/FlightResults.js
+++ demos/client/src/components/FlightResults.js
@@ -6,7 +6,7 @@ export default () => (
     <div>
         <h2>Flight Results</h2>
 
-        <FlightCell changePriceSection={travelOptions.get('price').get(1)}
+        <FlightCell changePriceSection={travelOptions.get('price').get(0)}
                     firstHour={travelOptions.get('hour').get(0)}
                     secondHour={travelOptions.get('hour').get(1)}  
                     firstCity={travelOptions.get('city').get(0)}    
@@ -18,7 +18,7 @@ export default () => (
                     firstCity={travelOptions.get('city').get(0)}    
                     secondCity={travelOptions.get('city').get(1)}/>
 
-        <FlightCell changePriceSection={travelOptions.get('price').get(1)}
+        <FlightCell changePriceSection={travelOptions.get('price').get(2)}
                     firstHour={travelOptions.get('hour').get(0)}
                     secondHour={travelOptions.get('hour').get(1)}  
                     firstCity={travelOptions.get('city').get(0)}    
