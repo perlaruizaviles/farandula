edc14b74fe8123b25d2112e9dd9a7ff087959ac9
Jhonatan Laguna <hola@jhonatanlaguna.com>
2017-05-17 12:08:05 -0700

    Update apiEndpoints.js

--- demos/client/src/api/apiEndpoints.js
+++ demos/client/src/api/apiEndpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://farandula-java.herokuapp.com/api';
+const BASE_URL = 'https://farandula-java.herokuapp.com/api/';
 
 export const AIRPORTS_URL = BASE_URL + 'airports';
 export const AVAILABLE_FLIGHTS_URL = BASE_URL + 'availableFlights';
