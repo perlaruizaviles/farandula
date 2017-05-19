708a01bea073a96a42e1eecc7adb84535c1f728e
Jhonatan Laguna <jhonlpdev@gmail.com>
2017-05-19 09:53:28 -0700

    [Fix] Deleted unused imports an libraries

--- demos/client/src/store.js
+++ demos/client/src/store.js
@@ -5,15 +5,9 @@ import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const user = {
-  firstName: '',
-  lastName: ''
-};
-
 const store = createStore(
     reducer,
     initialState,
-    user,
     composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
