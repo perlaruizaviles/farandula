d125b5cdfe4e00dceb170a03e3e3fa4dc2f028f3
emoteNS <emote@nearsoft.com>
2017-05-22 13:17:25 -0500

    Flight Service test changed.

--- demos/java-server/src/test/java/com/farandula/Service/FlightServiceTest.java
+++ demos/java-server/src/test/java/com/farandula/Service/FlightServiceTest.java
@@ -143,9 +143,10 @@ public class FlightServiceTest {
         List<FlightItinerary> flightItineraries = flightService.getFlightItineraryFromItinerary(itineraryList, "oneWay");
 
         assertEquals(itineraryList.size(), flightItineraries.size());
-        assertEquals(itineraryList.get(0).getAirlegs().get(0).getDepartureAirportCode(),
-                flightItineraries.get(0).getAirlegs().get(0).getDepartureAirport().getIata());
 
-
+        for (int i = 0; i < flightItineraries.size(); i++){
+            assertEquals(itineraryList.get(i).getAirlegs().get(0).getDepartureAirportCode(),
+                    flightItineraries.get(i).getAirlegs().get(0).getDepartureAirport().getIata());
+        }
     }
 }
