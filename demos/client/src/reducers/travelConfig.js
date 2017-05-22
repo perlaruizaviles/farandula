4054f66f792685cd8f6012568b1732469e5cc7fd
abustamante <abustamante@nearsoft.com>
2017-05-22 15:58:33 -0700

    hot-fix

--- demos/client/src/reducers/travelConfig.js
+++ demos/client/src/reducers/travelConfig.js
@@ -8,12 +8,13 @@ const travelConfig = (state = Map({}), action) => {
       return state.set('type', action.value);
 
     case types.CHANGE_TRAVEL_DATE:
+    debugger
       if(!state.getIn(['dates', 'depart']) && !state.getIn(['dates', 'return'])) {
         state = state.setIn(['dates', 'depart'], action.date);
         state = state.setIn(['dates', 'return'], action.date);
         return state;
       }
-      if(state.get('type')==='round-trip') {
+      if(state.get('type')==='roundTrip') {
         if(action.dateType==='depart'){
           if(action.date > state.getIn(['dates','return'])) {
             state = state.setIn(['dates','return'], action.date);
