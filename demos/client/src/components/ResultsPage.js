d7eb61ca9facf6522541d6b92e57c4b968f68937
aabustamante <abustamante@nearsoft.com>
2017-05-23 01:31:10 -0700

    fix router

---
+++ demos/client/src/components/ResultsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ItineraryList from '../containers/ItineraryList';
+import TravelSearch from '../containers/TravelSearch';
+import {Container} from 'semantic-ui-react';
+
+const TravelResults = () => {
+    return (
+        <Container>
+            <TravelSearch/>
+            <ItineraryList />
+        </Container>
+    )
+}
+
+export default TravelResults;
--- demos/client/src/router.js
+++ demos/client/src/router.js
@@ -3,18 +3,16 @@ import {syncHistoryWithStore} from 'react-router-redux';
 import {Router, Route, browserHistory, IndexRedirect} from 'react-router';
 
 import Main from './components/Main';
-import Home from './components/Home';
 import Summary from './components/Summary';
-import TravelResults from './containers/TravelResults';
+import ResultsPage from './components/ResultsPage';
 
 const makeRouter = store => {
   let history = syncHistoryWithStore(browserHistory, store);
   return (
     <Router history={history}>
       <Route path="/" component={Main}>
-        <IndexRedirect to="/travelResults"/>
-        <Route path="/home" component={Home}/>
-        <Route path="/travelResults" component={TravelResults}/>
+        <IndexRedirect to="/resultsPage"/>
+        <Route path="/resultsPage" component={ResultsPage}/>
         <Route path="/summary" component={Summary}/>
       </Route>
     </Router>
