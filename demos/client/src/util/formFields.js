10d807f6ac70f7cb500f0c3c60a8224837e2e292
Jhonatan Laguna <jhonlpdev@gmail.com>
2017-05-24 11:59:04 -0700

    Added validation to select field on forms

--- demos/client/src/util/formFields.js
+++ demos/client/src/util/formFields.js
@@ -18,10 +18,37 @@ const renderFieldWithError = (input, label, type, touched, error) => (
   </div>
 );
 
+const renderSelectFieldWithoutError = ( input, label, type, children ) => (
+  <div className="field">
+    <label>{label}</label>
+    <div>
+      <select {...input}>
+        {children}
+      </select>
+    </div>
+  </div>
+);
 
-export const renderField = ({input, label, type, meta: {touched, error}}) => {
-  if (touched && (error)) {
+const renderSelectFieldWithError = ( input, label, type,touched,error, children ) => (
+  <div className="error field">
+    <label>{label} - <i aria-hidden="true" className="red warning sign icon"/>{error}</label>
+    <div>
+      <select {...input}>
+        {children}
+      </select>
+    </div>
+  </div>
+);
+
+export const renderField = ({input, label, type, meta: {touched, error}, children}) => {
+
+  if(type === "select"){
+    if (touched && (error)) {
+      return renderSelectFieldWithError(input, label, type, touched, error, children);
+    }
+    return renderSelectFieldWithoutError(input, label, type, children);
+  }else if (touched && (error)) {
     return renderFieldWithError(input, label, type, touched, error);
   }
-  return renderFieldWithoutError(input, label, type)
+  return renderFieldWithoutError(input, label, type);
 };
