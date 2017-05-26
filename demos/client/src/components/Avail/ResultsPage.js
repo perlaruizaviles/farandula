1160eefbffaef68c0c98c748dd887ffa60da8e41
abustamante <abustamante@nearsoft.com>
2017-05-26 11:02:07 -0700

    Remove ItineraryFilterCard component

--- demos/client/src/components/Avail/ResultsPage.js
+++ demos/client/src/components/Avail/ResultsPage.js
@@ -1,22 +1,32 @@
 import React from "react";
 import ItineraryList from "../../containers/ItineraryList";
 import TravelSearch from "../../containers/TravelSearch";
-import ItineraryFilterCard from "./ItineraryFilterCard";
-import {Container, Grid} from "semantic-ui-react";
+import {Container, Grid, Card} from "semantic-ui-react";
 
 const TravelResults = () => {
   return (
     <Container>
-      <TravelSearch/>
       <Grid>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <ItineraryFilterCard/>
-          </Grid.Column>
-          <Grid.Column width={13}>
+        <Grid.Column width={15} floated='right'>
+          <Grid.Row>
+            <TravelSearch/>
+          </Grid.Row>
+          <Grid.Row>
             <ItineraryList />
-          </Grid.Column>
-        </Grid.Row>
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column width={1} floated='left'>
+          <Card className='orange fixed'>
+            <Card.Content>
+              <Card.Header>
+                Filters
+              </Card.Header>
+            </Card.Content>
+            <Card.Content>
+              Aqui iran los filtros
+            </Card.Content>
+          </Card>
+        </Grid.Column>
       </Grid>
     </Container>
   )
