ba5be78e89195eb7af3b1a5ff1e39821feca627f
Jhonatan Laguna <jhonlpdev@gmail.com>
2017-05-26 21:11:56 -0700

    [Fix]Routes redirect on unknown path

--- demos/client/src/router.js
+++ demos/client/src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {syncHistoryWithStore} from "react-router-redux";
-import {browserHistory, IndexRedirect, Route, Router} from "react-router";
+import {browserHistory, Redirect, Route, Router} from "react-router";
 import * as routes from "./routes";
 
 import Main from "./components/Main";
@@ -12,11 +12,10 @@ const makeRouter = store => {
   return (
     <Router history={history}>
       <Route path="/" component={Main}>
-        <IndexRedirect to={routes.HOME}/>
         <Route path={routes.HOME} component={ResultsPage}/>
         <Route path={routes.SUMMARY} component={Summary}/>
-        <Route path={routes.RESULTS} component={ResultsPage}/>
       </Route>
+      <Redirect from='*' to={routes.HOME}/>
     </Router>
   )
 };
