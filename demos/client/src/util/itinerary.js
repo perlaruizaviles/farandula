f0016c1fa25b5039857d074fbac1fc8c847acf53
jluque <jluque@nearsoft.com>
2017-06-07 10:01:34 -0700

    modify itinerary function from util

--- demos/client/src/util/itinerary.js
+++ demos/client/src/util/itinerary.js
@@ -1,22 +1,20 @@
 export const airlineNameByAirlegs = airlegs => {
-	let result;
 
-	airlegs.map((airleg) => {
-		let airline;
-		return(
-			airleg.segments.map((segment) => {
-				if(!airline){
-					airline = segment.airLineMarketingName;
-					result = airline;
-				}else{
-					if(airline!==segment.airLineMarketingName) {
-						result = "multiple airlines"
-						return result;
-					}
-				}
-				return result;
-			})
-		);
-	})
-	return result;
+  let airline;
+
+  airlegs.map((airleg) => {
+    return (
+      airleg.segments.map((segment) => {
+        if (!airline) {
+          airline = segment.airLineMarketingName;
+        } else {
+          if (airline !== segment.airLineMarketingName) {
+            airline = "multiple airlines"
+          }
+        }
+        return airline;
+      })
+    );
+  })
+  return airline;
 }
