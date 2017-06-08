e5d803829e3881f53afd7f8b53e9b9642830f430
juan <jluque@nearsoft.com>
2017-06-08 15:22:04 -0700

    Clean up

--- demos/client/src/util/dates.js
+++ demos/client/src/util/dates.js
@@ -1,5 +1,4 @@
 import moment from "moment";
-import {Map} from 'immutable';
 
 export const diffFormatted = (departureDate, arrivalDate) => {
   let totalHours = moment.unix(arrivalDate).diff(moment.unix(departureDate), 'hours');
@@ -12,23 +11,3 @@ export const formatTime = (Time) => {
   let timeFormatted = moment.unix(Time);
   return timeFormatted.format('h:mmp');
 };
-
-const changeRangeDate = (dateOne, dateTwo, shouldDateOneBeFirst = true) => {
-  if (dateOne.diff(dateTwo) > 0){
-    return (shouldDateOneBeFirst)? 
-      Map({'depart':dateOne, 'return':dateOne}) : 
-      Map({'depart':dateTwo, 'return':dateTwo})
-  } else {
-    return Map({'depart':dateOne, 'return':dateTwo});
-  }
-}
-
-export const changeTravelDates = (dates, newDate, dateType) => {
-  if(dates.get('depart') == null && dates.get('return') == null) return Map({'depart': newDate, 'return': newDate})
-  if(dateType==='depart'){
-    dates = changeRangeDate(newDate, dates.get('return'));
-  } else if (dateType==='return'){
-    dates = changeRangeDate(dates.get('depart'), newDate, false);
-  }
-  return dates;
-}
