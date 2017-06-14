bf27ad76b8c8bed3835ce54f62170f2e3cd9ef67
Enrique Mote <emote@nearsoft.com>
2017-06-14 18:24:29 -0500

    POM modified. Added plugins to sign and deploy artifacts

--- farandula-java/src/main/java/com/nearsoft/farandula/flightmanagers/FlightManager.java
+++ farandula-java/src/main/java/com/nearsoft/farandula/flightmanagers/FlightManager.java
@@ -11,8 +11,28 @@ import java.util.List;
 
 public interface FlightManager {
 
+    /**
+     * Returns a List of Itinerary elements starting from a search command object. This method
+     * has implementetion on
+     * {@link com.nearsoft.farandula.flightmanagers.amadeus.AmadeusFlightManager#getAvail(SearchCommand)
+     * AmadeusFlightManager.getAvail(SearchCommand search)},
+     * {@link com.nearsoft.farandula.flightmanagers.travelport.TravelportFlightManager#getAvail(SearchCommand)
+     * TravelportFlightManager.getAvail(SearchCommand search)} and
+     * {@link com.nearsoft.farandula.flightmanagers.sabre.SabreFlightManager#getAvail(SearchCommand)
+     * SabreFlightManager.getAvail(SearchCommand search)}
+     *
+     * @param search SearchCommand
+     * @return List<Itinerary>
+     * @throws FarandulaException
+     * @throws IOException
+     */
     List<Itinerary> getAvail(SearchCommand search) throws FarandulaException, IOException;
 
+    /**
+     *
+     * @param date LocalDateTime
+     * @throws FarandulaException
+     */
     default void validateDate(LocalDateTime date) throws FarandulaException {}
     //get seats
 
