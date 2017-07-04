6a6a16ff1dd500f4db076b1d444db0312f1e2395
aabustamante <abustamante@nearsoft.com>
2017-07-03 22:23:28 -0700

    Modify gitignore

--- farandula-js/build/models/multiCityTravel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MultiCityTravel {
-    constructor(travel) {
-        Object.assign(this, travel);
-    }
-}
-exports.MultiCityTravel = MultiCityTravel;
-//# sourceMappingURL=/home/alan/projects/farandula/farandula-js/models/multiCityTravel.js.map
