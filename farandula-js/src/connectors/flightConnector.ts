9c8cfa3dae105547450633791fb8cda9bf6de5e0
abustamante <abustamante@nearsoft.com>
2017-07-06 10:52:05 -0700

    Delete some now obsolete classes and interfaces

--- farandula-js/src/connectors/flightConnector.ts
+++
@@ -1,5 +0,0 @@
-import { Travel } from '../models/travel'
-export interface FlightConnector {
-  url: string
-	getAvails(travel:Travel<any>): any //hay que cambiar el tipo cuando se creen los modelos => list<itineraries> ?
-}
