9c03fe72f2845c56b2b9cdf97873d97eecdcbd55
Enrique Mote <emote@nearsoft.com>
2017-07-10 15:55:30 -0500

    Build head and search airleg parts for xml request. Added tests

--- farandula-ruby/lib/farandula/flight_managers/travelport/request.rb
+++ farandula-ruby/lib/farandula/flight_managers/travelport/request.rb
@@ -1,4 +1,3 @@
-require 'erb'
 module Farandula
   module FlightManagers
     module Travelport
@@ -10,20 +9,34 @@ module Farandula
 
         def get_head(target_branch)
           str = File.read(File.dirname(__FILE__)+'/../../assets/travelport/requestHeader.xml')
-          #result = eval("\""+str+"\"")
+          replace_string str, {target_branch: target_branch}
+        end
 
+        def get_airlegs(search_form)
 
+          str = File.read(File.dirname(__FILE__)+'/../../assets/travelport/requestSearchAirleg.xml')
+
+          result = ""
+
+          search_form.departure_airport.each_with_index {|leg, i|
+
+            map = {
+                departure_airport:  search_form.departure_airport[i],
+                arrival_airport:    search_form.arrival_airport[i],
+                departure_date:     search_form.departing_date[i],
+                class_travel:       search_form.cabin_class.upcase
+            }
+
+            result += replace_string str, map
+          }
+
+          result
 
         end
 
+        private
         def replace_string(file, params)
-          params
-          template = ""
-          open(file) { |f|
-            template = f.to_a.join
-          }
-
-          updated = ERB.new(template, 0, "%<>").result
+          file % params
         end
 
       end
--- farandula-ruby/test/unit/flight_managers/travelport/travelport_request_test.rb
+++ farandula-ruby/test/unit/flight_managers/travelport/travelport_request_test.rb
@@ -3,6 +3,7 @@ require 'file_helper'
 require 'string_helper'
 require 'minitest/autorun'
 require 'farandula/flight_managers/travelport/request'
+require 'farandula/models/search_form'
 
 class Farandula::TravelportRequestTest < Minitest::Test
   
@@ -15,12 +16,46 @@ class Farandula::TravelportRequestTest < Minitest::Test
 
   def test_get_head()
     request_result = @request.get_head('P105356')
-    expected = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-  <soapenv:Header/>
-  <soapenv:Body>
-    <air:LowFareSearchReq xmlns:air="http://www.travelport.com/schema/air_v34_0" AuthorizedBy="user" SolutionResult="true" TargetBranch="P105356" TraceId="trace">
-      <com:BillingPointOfSaleInfo xmlns:com="http://www.travelport.com/schema/common_v34_0" OriginApplication="UAPI"/>'
-    assert_equal(request_result, expected)
+    expected =  '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' + "\n" +
+                '  <soapenv:Header/>' + "\n" +
+                '  <soapenv:Body>' + "\n" +
+                '    <air:LowFareSearchReq xmlns:air="http://www.travelport.com/schema/air_v34_0" AuthorizedBy="user" SolutionResult="true" TargetBranch="P105356" TraceId="trace">' + "\n" +
+                '      <com:BillingPointOfSaleInfo xmlns:com="http://www.travelport.com/schema/common_v34_0" OriginApplication="UAPI"/>'
+
+    assert_equal(expected, request_result)
+  end
+
+  def test_get_airleg()
+
+    search_form = Farandula::SearchForm.new ["DFW"],
+                                                ["CDG"],
+                                                [Date.new(2017,07,10)],
+                                                [],
+                                                {},
+                                                :oneway,
+                                                :economy,
+                                                nil
+
+
+    airleg_result = @request.get_airlegs search_form ;
+
+    expected =  '<air:SearchAirLeg>' + "\n" +
+                '  <air:SearchOrigin>' + "\n" +
+                '    <com:Airport Code="DFW" xmlns:com="http://www.travelport.com/schema/common_v34_0"/>' + "\n" +
+                '  </air:SearchOrigin>' + "\n" +
+                '  <air:SearchDestination>' + "\n" +
+                '    <com:Airport Code="CDG" xmlns:com="http://www.travelport.com/schema/common_v34_0"/>' + "\n" +
+                '  </air:SearchDestination>' + "\n" +
+                '  <air:SearchDepTime PreferredTime="2017-07-10"/>' + "\n" +
+                '  <air:AirLegModifiers>' + "\n" +
+                '    <air:PreferredCabins>' + "\n" +
+                '      <com:CabinClass Type="ECONOMY" xmlns:com="http://www.travelport.com/schema/common_v34_0"/>' + "\n" +
+                '    </air:PreferredCabins>' + "\n" +
+                '  </air:AirLegModifiers>' + "\n" +
+                '</air:SearchAirLeg>'
+
+    assert_equal expected, airleg_result
+
   end
 
 end
