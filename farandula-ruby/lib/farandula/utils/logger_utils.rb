36e0f5767b5957ec91da3c6c439e484ed25e8905
abustamante <abustamante@nearsoft.com>
2017-07-24 14:39:20 -0700

    fix get_pretty_xml method and its respective test

--- farandula-ruby/lib/farandula/utils/logger_utils.rb
+++ farandula-ruby/lib/farandula/utils/logger_utils.rb
@@ -1,21 +1,16 @@
 require 'json'
-require 'rexml/document'
 
 module Farandula
   module Utils
     class LoggerUtils
-
       def self.get_pretty_json(ugly_json_string)
         json_object = JSON.parse(ugly_json_string)
         JSON.pretty_generate(json_object)
       end
 
-      def self.get_pretty_xml(input, indent)
-        result = ""
-        doc = REXML::Document.new(input).write(:output => result, :indent => indent)
-        result.gsub("'",'"')
+      def self.get_pretty_xml(input)
+        Nokogiri::XML(input).to_xml
       end
-
     end
   end
 end
--- farandula-ruby/test/unit/utils/logger_utils_test.rb
+++ farandula-ruby/test/unit/utils/logger_utils_test.rb
@@ -4,58 +4,47 @@ require 'string_helper'
 require 'minitest/autorun'
 require 'farandula/utils/logger_utils'
 
-class Farandula::LoggerUtilsTest < Minitest::Test
+module Farandula
+  class LoggerUtilsTest < Minitest::Test
+    include Farandula::Utils
+    def test_logger_pretty_json
+      ugly_json = '{"b":42.005,"a":[42,17],"longer":true,"str":"yes please"}'
+      expected = '{' + "\n" \
+                  '  "b": 42.005,' + "\n" \
+                  '  "a": [' + "\n" \
+                  '    42,' + "\n" \
+                  '    17' + "\n" \
+                  '  ],' + "\n" \
+                  '  "longer": true,' + "\n" \
+                  '  "str": "yes please"' + "\n" \
+                  '}'
 
-  include Farandula::Utils
-
-  def test_logger_pretty_json
-    ugly_json = '{"b":42.005,"a":[42,17],"longer":true,"str":"yes please"}'
+      actual = LoggerUtils.get_pretty_json(ugly_json)
+      assert_equal(
+        expected,
+        actual
+      )
+    end
     
-    expected = '{'+"\n"+
-                '  "b": 42.005,'+"\n"+
-                '  "a": ['+"\n"+
-                '    42,'+"\n"+
-                '    17'+"\n"+
-                '  ],'+"\n"+
-                '  "longer": true,'+"\n"+
-                '  "str": "yes please"'+"\n"+
-                '}'
+    def test_logger_pretty_xml
+      ugly_xml = '<?xml version="1.0" encoding="UTF-8"?><note><to>Nearsoft.inc</to><from>Quantums-Team</from>' \
+        '<heading>Greetings</heading>' \
+        '<body>Hello, nearsoft!</body></note>'
 
-    actual = LoggerUtils.get_pretty_json(ugly_json)
-    
-    assert_equal(
-      expected,
-      actual
-    )
-  end
-  
-  def test_logger_pretty_xml
+      expected =  '<?xml version="1.0" encoding="UTF-8"?>' + "\n" \
+                  '<note>' + "\n" \
+                  '  <to>Nearsoft.inc</to>' + "\n" \
+                  '  <from>Quantums-Team</from>' + "\n" \
+                  '  <heading>Greetings</heading>' + "\n" \
+                  '  <body>Hello, nearsoft!</body>' + "\n" \
+                  '</note>' + "\n"
 
-    ugly_xml = '<?xml version="1.0" encoding="UTF-8"?><note><to>Nearsoft.inc</to><from>Quantums-Team</from>'+
-      '<heading>Greetings</heading>'+
-      '<body>Hello, nearsoft!</body></note>'
+      actual = LoggerUtils.get_pretty_xml(ugly_xml)
 
-    expected =  '<?xml version="1.0" encoding="UTF-8"?>' + "\n" +
-                '<note>' + "\n" +
-                '  <to>' + "\n" +
-                '    Nearsoft.inc' + "\n" +
-                '  </to>' + "\n" +
-                '  <from>' + "\n" +
-                '    Quantums-Team' + "\n" +
-                '  </from>' + "\n" +
-                '  <heading>' + "\n" +
-                '    Greetings' + "\n" +
-                '  </heading>' + "\n" +
-                '  <body>' + "\n" +
-                '    Hello, nearsoft!' + "\n" +
-                '  </body>' + "\n" +
-                '</note>'
-
-    actual = LoggerUtils.get_pretty_xml(ugly_xml, 2)
-
-    assert_equal(
-      expected,
-      actual
-    )
+      assert_equal(
+        expected,
+        actual
+      )
+    end
   end
 end
