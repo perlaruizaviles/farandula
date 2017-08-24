10cd6a9c6d7c53c6cd40873c3d113101973792ca
Perla Ruiz <pruiz@NSL-PRUIZs-MacBook-Pro.local>
2017-08-24 12:12:28 -0700

    Improving files structure.

---
+++ samples/ruby-server/lib/helpers/DateParser.rb
@@ -0,0 +1,8 @@
+module Farandula
+	class DateParser
+
+		def dateToTimesstampSeconds(date)
+			return date
+		end
+	end
+end
---
+++ samples/ruby-server/lib/helpers/assets.rb
@@ -0,0 +1,8 @@
+module Farandula
+  class Assets
+
+    def self.load_file(file_name)
+      File.read( File.dirname(__FILE__) + '/../../assets/' + file_name  )
+    end
+  end
+end
